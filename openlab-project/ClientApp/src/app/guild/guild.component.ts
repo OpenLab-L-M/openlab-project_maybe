4e9e4a7761c2a35a5d5f1df1faa4e110e152c490
GulityCrown <cupalukas2@gmail.com>
2023-11-08 22:30:58 +0100

    dobehli sme

--- openlab-project/ClientApp/src/app/guild/guild.component.ts
+++ openlab-project/ClientApp/src/app/guild/guild.component.ts
@@ -1,27 +1,58 @@
-import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  updateNumber(userId: number, number: number) {
+    return this.http.put<void>('users' + userId, { number });
+  }
+
+  updateGuildInformationNumber(guildId: number, userId: number, number: number) {
+    return this.http.put<void>('guilds' + guildId + '/join', { userId, number });
+  }
+}
+
 
 @Component({
   selector: 'app-guild',
   templateUrl: './guild.component.html',
   styleUrls: ['./guild.component.css']
 })
-export class GuildComponent {
-  public guildInfo!: GuildDTO;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<GuildDTO>(baseUrl + 'guild').subscribe(result => {
-      this.guildInfo = result;
+export class GuildComponent {
+
+  Name: string = "no data";
+  Description: string = "no data";
+  GuildMaxMembers: number = 0;
+  MembersCount: number = 0;
+
+  public GuildData: GuildInformation[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, @Inject(UserService) private userService: UserService) {
+    http.get<GuildInformation[]>(baseUrl + 'guild').subscribe(result => {
+      this.GuildData = result;
+
     }, error => console.error(error));
+  }
+
+  async onJoinClick(guildId: number, userId: number) {
+
+    await this.userService.updateGuildInformationNumber(guildId, userId, 1);
   }
 }
 
-export interface GuildDTO {
-  Name: string,
-  Description: string,
-  GuildMaxMembers: number,
-  GuildMembers: number,
-  MembersCount: number,
-  GuildInformation: string
-
+interface GuildInformation {
+  name: string;
+  id: number;
+  description: string;
+  guildMaxMembers: number;
+  membersCount: number;
+  userId?: number;
 }
 
